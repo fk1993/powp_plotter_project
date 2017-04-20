0f7ed8145e6f2928a223c5eea7cd3037a49fd42c
fk1993 <fkaszuba93@gmail.com>
2017-04-20 15:40:50 +0200

    lab2 3.1.4.

---
+++ src/main/java/edu/iis/powp/command/CommandFactory.java
@@ -0,0 +1,34 @@
+package edu.iis.powp.command;
+
+import java.awt.Point;
+
+public class CommandFactory {
+
+	public static ComplexCommand getRectangleCommand(int x, int y, int width, int height){
+		ComplexCommand cmd = new ComplexCommand();
+		cmd.add(new CommandSetPosition(x, y));
+		cmd.add(new CommandDrawLineToPosition(x + width, y));
+		cmd.add(new CommandDrawLineToPosition(x + width, y + height));
+		cmd.add(new CommandDrawLineToPosition(x, y + height));
+		cmd.add(new CommandDrawLineToPosition(x, y));
+		return cmd;
+	}
+	
+	public static ComplexCommand getTriangleCommand(int x1, int y1, int x2, int y2, int x3, int y3){
+		ComplexCommand cmd = new ComplexCommand();
+		cmd.add(new CommandSetPosition(x1, y1));
+		cmd.add(new CommandDrawLineToPosition(x2, y2));
+		cmd.add(new CommandDrawLineToPosition(x3, y3));
+		cmd.add(new CommandDrawLineToPosition(x1, y1));
+		return cmd;
+	}
+	
+	public static ComplexCommand getPolygonCommand(int x, int y, Point... points){
+		ComplexCommand cmd = new ComplexCommand();
+		cmd.add(new CommandSetPosition(x, y));
+		for(Point p: points)
+			cmd.add(new CommandDrawLineToPosition((int)p.getX(), (int)p.getY()));
+		cmd.add(new CommandDrawLineToPosition(x, y));
+		return cmd;
+	}
+}
